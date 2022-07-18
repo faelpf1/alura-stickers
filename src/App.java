63c6c9fc366c96fbf9a3d9a91ac8769275e36682
Rafael <rafaelpfilgueiras@gmail.com>
2022-07-18 18:54:49 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("1-Top 250 Filmes \n2-Filmes mais populares \n3-Top 250 Séries \n4-Séries mais populares");
+        System.out.println("Digite uma das opções: ");
+        Scanner opcao = new Scanner(System.in);
+
+        EndpointIMDB endpoint = new EndpointIMDB();
+
+        switch (opcao.nextInt()){
+            case 1:
+                //endpoint.endPoint("https://imdb-api.com/en/API/Top250Movies/k_a83ddg3k");
+                endpoint.endPoint("https://api.mocki.io/v2/549a5d8b");
+                break;
+            case 2:
+                endpoint.endPoint("https://imdb-api.com/en/API/MostPopularMovies/k_a83ddg3k");
+                break;
+            case 3:
+                endpoint.endPoint("https://imdb-api.com/en/API/Top250TVs/k_a83ddg3k");
+                break;
+            case 4:
+                endpoint.endPoint("https://imdb-api.com/en/API/MostPopularTVs/k_a83ddg3k");
+                break;
+            default:
+                System.out.println("Opção invalida!");
+        }
+    }
+}
---
+++ src/EndpointIMDB.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class EndpointIMDB {
+
+    public void endPoint(String url) throws IOException, InterruptedException{
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+        // extrair só os dados que interessam ( titulos, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        // exibir e manipular os dados 
+        for (Map<String, String> filme: listaDeFilmes){
+            System.out.println("\u001b[1m" + "Título: \u001b[m" + "\u001b[3m"+ filme.get("title") + "\u001b[m");
+            System.out.println("\u001b[1m" + "Poster: \u001b[m" + filme.get("image"));
+            
+            String rate = filme.get("imDbRating");
+            System.out.println("\u001b[45m" +  "Classificação: " + rate + "\u001b[m");
+            String stars = "";
+            for (int i=0; i<Math.round(Float.parseFloat(rate)); i++){
+                stars += "\u2B50";
+            }
+            System.out.println(stars);
+            System.out.println();
+        }
+
+    
+    }
+    
+}
