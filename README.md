89b4db6793a6737624fedfc27ac6b41a78bb2485
Rafael <rafaelpfilgueiras@gmail.com>
2022-07-18 21:09:19 -0300

    chave da API colocada na variavel de ambiente

--- README.md
+++ README.md
@@ -10,6 +10,6 @@ Esse projeto é um desafio realizado durante a imersão java_ da Alura, onde foi
 ## Deafios a serem desenvolvidos
 - [x] Consumir o endpoint de filmes mais populares da API do IMDB. Procure também, na documentação da API do IMDB, o endpoint que retorna as melhores séries e o que retorna as séries mais populares.
 - [x] Usar sua criatividade para deixar a saída dos dados mais bonitinha: usar emojis com código UTF-8, mostrar a nota do filme como estrelinhas, decorar o terminal com cores, negrito e itálico usando códigos ANSI, e mais!
-- [ ] Colocar a chave da API do IMDB em algum lugar fora do código como um arquivo de configuração (p. ex, um arquivo .properties) ou uma variável de ambiente.
+- [x] Colocar a chave da API do IMDB em algum lugar fora do código como um arquivo de configuração (p. ex, um arquivo .properties) ou uma variável de ambiente.
 - [ ] Mudar o JsonParser para usar uma biblioteca de parsing de JSON como Jackson ou GSON.
 - [ ] Desafio supremo: criar alguma maneira para você dar uma avaliação ao filme, puxando de algum arquivo de configuração OU pedindo a avaliação para o usuário digitar no terminal.
--- src/App.java
+++ src/App.java
@@ -9,21 +9,23 @@ public class App {
 
         EndpointIMDB endpoint = new EndpointIMDB();
 
+        String apikey = System.getenv( "API_KEY");
+
         switch (opcao.nextInt()){
             case 1:
-                //endpoint.endPoint("https://imdb-api.com/en/API/Top250Movies/k_a83ddg3k");
+                //endpoint.endPoint("https://imdb-api.com/en/API/Top250Movies/" + apikey);
                 endpoint.endPoint("https://api.mocki.io/v2/549a5d8b");
                 break;
             case 2:
-                //endpoint.endPoint("https://imdb-api.com/en/API/MostPopularMovies/k_a83ddg3k");
+                //endpoint.endPoint("https://imdb-api.com/en/API/MostPopularMovies/" + apikey);
                 endpoint.endPoint("https://api.mocki.io/v2/549a5d8b/MostPopularMovies");
                 break;
             case 3:
-                //endpoint.endPoint("https://imdb-api.com/en/API/Top250TVs/k_a83ddg3k");
+                //endpoint.endPoint("https://imdb-api.com/en/API/Top250TVs/" + apikey);
                 endpoint.endPoint("https://api.mocki.io/v2/549a5d8b/Top250TVs");
                 break;
             case 4:
-                //endpoint.endPoint("https://imdb-api.com/en/API/MostPopularTVs/k_a83ddg3k");
+                //endpoint.endPoint("https://imdb-api.com/en/API/MostPopularTVs/" + apikey);
                 endpoint.endPoint("https://api.mocki.io/v2/549a5d8b/MostPopularTVs");
                 break;
             default:
