c9b266ea5cd6347f80c57f3d21a8546ee5eb6738
Rafael <rafaelpfilgueiras@gmail.com>
2022-07-21 20:04:48 -0300

    init

--- linguagens-api2/linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.linguagens.api;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
-    
-}
